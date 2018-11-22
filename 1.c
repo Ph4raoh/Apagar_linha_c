e0a6946a83b7bf549df67de68534af0b9ae507e9
Ph4ra0h <Wesley.jonatha2047@gmail.com>
2018-11-22 17:24:00 -0300

    0.0.1

---
+++ 1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+  // criando a variável ponteiro para o arquivo
+  FILE *orig,*saida;
+
+  int linha_atual = 0, linha_selecionada = 0;
+  char texto[55] = "";
+  
+  //abrindo o arquivo
+  orig = fopen("original.txt", "r");
+  saida = fopen("saida.txt", "w");
+
+scanf("%d",&linha_selecionada);
+
+
+   while(fgets(texto, 55, orig) != NULL){
+        if(linha_atual != (linha_selecionada - 1)){
+            fputs(texto, saida);
+              linha_atual += 1;
+        }
+        
+        
+}
+  
+  // fechando arquivo
+  fclose(orig);
+  fclose(saida);
+  
+  //mensagem para o usuário
+  printf("O arquivo foi criado com sucesso!\n");
+
+printf("linha_atual: %d\n",linha_atual);
+printf("linha_selecionada: %d\n",linha_selecionada);
+  
+
+  return(0);
+}
